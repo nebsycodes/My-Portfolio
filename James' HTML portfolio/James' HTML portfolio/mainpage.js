3a65e0be356b36d4e235aa2bbc45a0842a9dcfa9
James <145176861+nebsycodes@users.noreply.github.com>
2024-03-18 10:12:16 +1100

    Add files via upload
    update 1

---
+++ James' HTML portfolio/James' HTML portfolio/mainpage.js
@@ -0,0 +1,23 @@
+/*night mode button
+const checkbox = document.querySelector('.checkbox');
+
+checkbox.addEventListener('change', function() {
+    if (this.checked) {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+});
+
+
+/* Mouse over gradient 
+const root = document.documentElement;
+
+document.addEventListener("mousemove", (evt) => {
+	let x = evt.clientX / innerWidth;
+	let y = evt.clientY / innerHeight;
+
+	root.style.setProperty("--mouse-x", x);
+	root.style.setProperty("--mouse-y", y);
+});
+*/
