7b11511ee117b593bbdc8ac5226d46fb9c48aeb7
James <145176861+nebsycodes@users.noreply.github.com>
2025-01-15 08:11:03 +1100

    removed folder.

---
+++ 3d.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/2r0gqn4PCs-FvqjN/scene.splinecode');
